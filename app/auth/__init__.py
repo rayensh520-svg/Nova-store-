ae08cd49a750102e416f2647f68b8caf66e52abd
rayensh520-svg <rayensh520@gmail.com>
2026-09-05 00:33:41 +0100

    Update __init__.py
    from flask import Blueprint
    
    
    auth_bp = Blueprint(
        "auth",
        __name__,
        url_prefix="/api/v1/auth"
    )

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,14 +1,8 @@
-from flask import Flask
-
-from config import Config
-from app.auth import auth_bp
-from app.auth import routes
+from flask import Blueprint
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    app.register_blueprint(auth_bp)
-
-    return app
+auth_bp = Blueprint(
+    "auth",
+    __name__,
+    url_prefix="/api/v1/auth"
+)
