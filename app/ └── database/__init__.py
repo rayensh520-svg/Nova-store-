faee6b7bb14c8038ea6d09e4ddeb2aec88566d73
rayensh520-svg <rayensh520@gmail.com>
2026-09-05 00:08:11 +0100

    Update __init__.py
    from flask import Flask
    
    from config import Config
    from app.auth import auth_bp
    from app.auth import routes
    from app.database import get_connection
    from app.database.init_db import initialize_database
    
    
    def create_app():
        app = Flask(__name__)
        app.config.from_object(Config)
    
        initialize_database()
    
        app.register_blueprint(auth_bp)
    
        return app

--- app/ └── database/__init__.py
+++ app/ └── database/__init__.py
@@ -1,4 +1,18 @@
-from .connection import get_connection
+from flask import Flask
+
+from config import Config
+from app.auth import auth_bp
+from app.auth import routes
+from app.database import get_connection
+from app.database.init_db import initialize_database
 
 
-__all__ = ["get_connection"]
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    initialize_database()
+
+    app.register_blueprint(auth_bp)
+
+    return app
