8d58501d2dedb859a3a1f00290a9fd44f2998c4f
rayensh520-svg <rayensh520@gmail.com>
2026-09-05 12:38:29 +0100

    Update app.py
    import os
    
    from app import create_app
    
    app = create_app()
    
    if __name__ == "__main__":
        port = int(os.environ.get("PORT", 5001))
    
        app.run(
            host="0.0.0.0",
            port=port,
            debug=False
        )

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from app import create_app
+import os
 
+from app import create_app
 
 app = create_app()
 
-
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+
     app.run(
         host="0.0.0.0",
-        port=5001,
-        debug=True
+        port=port,
+        debug=False
     )
